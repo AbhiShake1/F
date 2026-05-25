beeadeddab290cbcfcbfbf608b67d6256cb87871
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2026-05-25 11:40:43 +0545

    chore: tighten help text to F [args...]

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import { setup } from './setup.js'
 const args = process.argv.slice(2)
 
 if (args.length === 0) {
-  process.stdout.write('F <file|url|string> [file|url|string ...]\n')
+  process.stdout.write('F [args...]\n')
   process.exit(0)
 }
 
